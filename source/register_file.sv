a7c02c56263ee421274b25e6be2acf683e0c4f8f
Bhakti Vora <bvora@purdue.edu>
2021-10-07 20:49:12 -0400

    synthesis working

--- source/register_file.sv
+++ source/register_file.sv
@@ -14,41 +14,28 @@ import cpu_types_pkg::*;
 word_t [31:0] regfile;
 word_t [31:0] regfile_next;
 
+//assign wires
+assign rfif.rdat1=regfile[rfif.rsel1];
+assign rfif.rdat2=regfile[rfif.rsel2];
+
 
 //comb block
 //keeping wdat ready by next clock cycle
 always_comb begin
-    if(!nRST)
-        regfile ='b0;
-    else if (rfif.WEN && (rfif.wsel=='b0))
-        regfile[0] =  'b0;
-    else if (rfif.WEN)
-        regfile[rfif.wsel]=rfif.wdat;
-
-    // regfile_next=regfile;
-    // if(nRST && rfif.wsel=='b0)
-    //     regfile_next[0]='b0;
-    // else if (nRST && rfif.wsel != 'b0)
-    //     regfile_next[rfif.wsel]= rfif.wdat;
+    regfile_next=regfile;
+    if(nRST && rfif.wsel=='b0)
+        regfile_next[0]='b0;
+    else if (nRST && rfif.wsel != 'b0)
+        regfile_next[rfif.wsel]= rfif.wdat;
 end
 //write at next clock cycle
-// always_ff @ (posedge CLK or negedge nRST)
-// begin
-//     if (!nRST)
-//         regfile='b0;
-//     else if (rfif.WEN)
-//         regfile<=regfile_next;
+always_ff @ (negedge CLK or negedge nRST)
+begin
+    if (!nRST)
+        regfile='b0;
+    else if (rfif.WEN)
+        regfile<=regfile_next;
    
-
-always @ (negedge CLK or negedge nRST) begin
-    if(!nRST) begin
-        rfif.rdat1<= 'b0;
-        rfif.rdat2<= 'b0;
-    end
-    else begin
-        rfif.rdat1<=regfile[rfif.rsel1];
-        rfif.rdat2<=regfile[rfif.rsel2];
-    end
 end
 
 endmodule
